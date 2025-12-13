a4332a42c6f9ab1aa3532c1b96786b4bc3d4cd96
Aleksandr Skoblin <aleksandr.skoblin05@gmail.com>
2025-12-13 11:53:57 +0300

    fixes

--- src/components/Pages/ProfilePage/index.tsx
+++ src/components/Pages/ProfilePage/index.tsx
@@ -1,13 +1,5 @@
 import Information from "../../Information";
 import "./style.css"
-import Logo1 from "/logo/head.png"
-import Logo2 from "/logo/head.png"
-import Logo3 from "/logo/head.png"
-import Logo4 from "/logo/head.png"
-import Logo5 from "/logo/head.png"
-import Logo6 from "/logo/head.png"
-import Logo7 from "/logo/head.png"
-import Logo8 from "/logo/head.png"
 import FaceLogo from "/logo/head.png"
 import {Link} from "react-router-dom";
 
@@ -32,19 +24,16 @@ const Index = () => {
                 </div>
                 <div className="first-settings">
                     <div className="profile-information">
-                        <img src={Logo1} alt=""/>
                         <Link to="/set-profile-information">Edit profile information</Link>
                     </div>
                     <div className="settings-notifications">
                         <div className="notifications">
-                            <img src={Logo2} alt=""/>
                             <button className="edit-notifications">Notifications</button>
                         </div>
                         <button className="current-notifications-state">On</button>
                     </div>
                     <div className="language">
                         <div className="language-settigs">
-                            <img src={Logo3} alt=""/>
                             <button className="edit-language">Language</button>
                         </div>
                         <button className="current-language-state">English</button>
@@ -52,12 +41,10 @@ const Index = () => {
                 </div>
                 <div className="second-settings">
                     <div className="security">
-                        <img src={Logo4} alt=""/>
                         <Link to="/security">Security</Link>
                     </div>
                     <div className="theme-settigs">
                         <div className="theme">
-                            <img src={Logo5} alt=""/>
                             <button className="edit-theme">Theme</button>
                         </div>
                         <button className="current-theme">Light Mode</button>
@@ -65,15 +52,12 @@ const Index = () => {
                 </div>
                 <div className="third-settings">
                     <div className="help">
-                        <img src={Logo6} alt=""/>
                         <Link to="/help">Help & Support</Link>
                     </div>
                     <div className="contact">
-                        <img src={Logo7} alt=""/>
                         <Link to="/contact">Contact Us</Link>
                     </div>
                     <div className="privacy">
-                        <img src={Logo8} alt=""/>
                         <Link to="/privacy">Privacy policy</Link>
                     </div>
                 </div>
--- src/components/Pages/WalletPage/index.tsx
+++ src/components/Pages/WalletPage/index.tsx
@@ -1,5 +1,12 @@
 import Information from "../../Information";
 import "./style.css"
+import ReceiveAmountImg  from "/logo/free-icon-receive-amount-7023614.png";
+import SendImg  from "/logo/free-icon-send-3106856.png";
+import DepositImg  from "/logo/free-icon-download-8208106.png";
+import Invoicing  from "/logo/free-icon-atm-machine-5941316.png";
+import Checkout  from "/logo/free-icon-eye-test-17977150.png";
+import BuyImg   from "/logo/free-icon-shopping-cart-760524.png";
+import MoreImg  from "/logo/free-icon-social-media-15051161.png";
 
 
 const Wallet = () => {
@@ -21,7 +28,68 @@ const Wallet = () => {
                     </div>
                     <div className="order-wallet-infromation">
                         <div className="markets">
-                            Markets
+                            <span className="HIGHLIGHT">Markets</span>
+                            <nav className="markets-options">
+                                <ol className="markets-options-list">
+                                    <li> <div className="markets-options-list-block">
+                                        <button className="markets-options-block">
+                                            <img src={DepositImg} alt=""/>
+                                        </button>
+                                        <div className="markets-options-block-name">
+                                            Deposit
+                                        </div>
+                                    </div></li>
+                                    <li> <div className="markets-options-list-block">
+                                        <button className="markets-options-block">
+                                            <img src={SendImg} alt=""/>
+                                        </button>
+                                        <div className="markets-options-block-name">
+                                            Send
+                                        </div>
+                                    </div></li>
+                                    <li> <div className="markets-options-list-block">
+                                        <button className="markets-options-block">
+                                            <img src={ReceiveAmountImg} alt=""/>
+                                        </button>
+                                        <div className="markets-options-block-name">
+                                            Receive
+                                        </div>
+                                    </div></li>
+                                    <li> <div className="markets-options-list-block">
+                                        <button className="markets-options-block">
+                                            <img src={Invoicing} alt=""/>
+                                        </button>
+                                        <div className="markets-options-block-name">
+                                            Invoicing
+                                        </div>
+                                    </div></li>
+                                    <li> <div className="markets-options-list-block">
+                                        <button className="markets-options-block">
+                                            <img src={Checkout} alt=""/>
+                                        </button>
+                                        <div className="markets-options-block-name">
+                                            Checkout
+                                        </div>
+                                    </div></li>
+                                    <li> <div className="markets-options-list-block">
+                                        <button className="markets-options-block">
+                                            <img src={BuyImg} alt=""/>
+                                        </button>
+                                        <div className="markets-options-block-name">
+                                            Buy
+                                        </div>
+                                    </div></li>
+                                    <li> <div className="markets-options-list-block">
+                                        <button className="markets-options-block">
+                                            <img src={MoreImg} alt=""/>
+                                        </button>
+                                        <div className="markets-options-block-name">
+                                            More
+                                        </div>
+                                    </div>
+                                    </li>
+                                </ol>
+                            </nav>
                         </div>
                         <div className="day-statystic">
                             <div className="last-days-information">
