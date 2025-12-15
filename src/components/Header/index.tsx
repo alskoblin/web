c24447e6616c3a53a5b5ea6bcf5cb24cd550114b
Aleksandr Skoblin <aleksandr.skoblin05@gmail.com>
2025-12-15 18:20:46 +0300

    fixes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,23 +1,37 @@
+import { useState } from "react";
 import Navigation from "../Navigation";
-import Logo from "/logo/toncoin-ton-logo.png"
-import './style.css'
-import { useLocation } from 'react-router-dom';
+import Logo from "/logo/toncoin-ton-logo.png";
+import "./style.css";
+import { useLocation } from "react-router-dom";
+
 const Header = () => {
     const location = useLocation();
+    const [open, setOpen] = useState(false);
 
-    const hideHeader = location.pathname === '/login';
+    if (location.pathname === "/login") return null;
 
-    if (hideHeader) {
-        return null;
-    }
     return (
-        <div className="header-container">
-            <div className="header__logo">
-                <img src={Logo} alt="" />
-                <div className="header__logo__name">Crypto</div>
+        <>
+            {/* БУРГЕР */}
+            <button
+                className={`burger ${open ? "active" : ""}`}
+                onClick={() => setOpen(!open)}
+            >
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
+
+            {/* SIDEBAR */}
+            <div className={`header-container ${open ? "active" : ""}`}>
+                <div className="header__logo">
+                    <img src={Logo} alt="" />
+                    <div className="header__logo__name">Crypto</div>
+                </div>
+                <Navigation />
             </div>
-            <Navigation />
-        </div>
-    )
-}
+        </>
+    );
+};
+
 export default Header;
